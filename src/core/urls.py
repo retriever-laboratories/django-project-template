33f6b4fea3c2736a6e55fde7095f9c42419601f7
Alex Tirado <fabian.alexis.th@gmail.com>
2026-06-11 16:31:55 -0400

    feat: base + navbar + sidebar

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from . import views
 
 def health(_):
     return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path("", views.index, name="dashboard"),
     # Admin site
     path("admin/", admin.site.urls),
 
