9d2fe87d61753f70575a040b4cb38a811ca4602d
Alex Tirado <fabian.alexis.th@gmail.com>
2026-06-15 19:03:10 -0400

    feat: change home view name back to "home"

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
-from . import views
+from core import views
 
 def health(_):
     return JsonResponse({"status": "ok"})
 
 urlpatterns = [
-    path("", views.index, name="dashboard"),
+    path("", views.index, name="home"),
+    
+    path("ping/", views.ping, name="ping"),
+    
     # Admin site
     path("admin/", admin.site.urls),
 
