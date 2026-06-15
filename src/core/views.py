8beebf43827eeed31d87fa4581e09aef7f673e8f
Alex Tirado <fabian.alexis.th@gmail.com>
2026-06-15 19:04:00 -0400

    feat: remove unnecessary path reference,  add back ping view function

--- src/core/views.py
+++ src/core/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "../templates/home.html")
+    return render(request, "home.html")
+
+def ping(request):
+    return render(request, "partials/ping.html")
