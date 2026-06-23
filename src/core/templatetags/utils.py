9827efbe849489f13a982ae8324506b596ecae7f
Sebastian Pinto <sebastian.guzman@gomoder.com>
2026-06-23 14:27:15 -0400

    feat: implement sorting render and improve filter badge rendering in table templates

--- src/core/templatetags/utils.py
+++ src/core/templatetags/utils.py
@@ -6,3 +6,13 @@ register = template.Library()
 @register.filter(name="getattr")
 def get_field(obj, field_name):
     return getattr(obj, field_name, "")
+
+
+@register.simple_tag
+def sort_direction(request, field_name):
+    for token in request.GET.getlist("o"):
+        if token == field_name:
+            return "asc"
+        if token == f"-{field_name}":
+            return "desc"
+    return None
