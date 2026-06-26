19c8b40d3ddff950baa177e0a699357d2f00a185
Sebastian Pinto <sebastian.guzman@gomoder.com>
2026-06-26 12:44:27 -0400

    feat: remove unused sorting and filtering functions from template tags and update related templates

--- src/core/templatetags/table_utils.py
+++ src/core/templatetags/table_utils.py
@@ -44,38 +44,3 @@ def getlist(querydict, key):
 @register.filter
 def verbose_name(model, field_name):
     return model._meta.get_field(field_name).verbose_name
-
-
-@register.simple_tag
-def sort_direction(request, field_name):
-    for order_field in request.GET.getlist("o"):
-        if order_field == field_name:
-            return "asc"
-        if order_field == f"-{field_name}":
-            return "desc"
-    return None
-
-
-@register.simple_tag
-def sort_params(request, field_name, direction):
-    order_field = field_name if direction == "asc" else f"-{field_name}"
-    current_order = request.GET.getlist("o")
-
-    already_selected = order_field in current_order
-    new_order = [entry for entry in current_order if entry.lstrip("-") != field_name]
-
-    if not already_selected:
-        new_order.append(order_field)
-
-    params = request.GET.copy()
-    params.setlist("o", new_order)
-
-    return params
-
-
-@register.simple_tag
-def remove_filter_params(request, key, value):
-    params = request.GET.copy()
-    params.setlist(key, [item for item in params.getlist(key) if item != value])
-
-    return params
